0b44f05149669f2228ecb42b5eb123748b10a9e2
Matheus Diniz <matheus.diniiiiz@gmail.com>
2022-07-03 17:27:00 +0100

    loading

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import Row from "./components/Row";
 import categories from "./api";
 import Banner from "./components/Banner";
 import Nav from "./components/Nav";
-import React, { useLayoutEffect, useState } from "react";
+// import React, { useLayoutEffect, useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-  useLayoutEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
+  // const [loading, setLoading] = useState(false);
+  // useLayoutEffect(() => {
+  //   setLoading(true);
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2000);
+  // }, []);
 
   return (
     <div className="App">
-      {loading ? (
-        <div className="loading">
+      {/* {loading ? ( */}
+        {/* <div className="loading">
           <img
             src="https://i.gifer.com/origin/36/36527397c208b977fa3ef21f68c0f7b2.gif"
             alt="Loading"/>
         </div>
-      ) : (
+      ) : ( */}
         <div>
           <Nav />
           <Banner />
@@ -37,7 +37,7 @@ function App() {
             );
           })}
         </div>
-      )}
+      {/* )} */}
     </div>
   );
 }
